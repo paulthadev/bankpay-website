fe3bac3380f960115760a169bbf6dce5a8c1935f
fadayopaul <paulfadayo@yahoo.com>
2022-09-26 14:26:42 +0100

    integrate tab component functionality

--- script.js
+++ script.js
@@ -99,5 +99,36 @@ btnLearnMore.addEventListener('click', function (e) {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
 
-////////////////////////////////////////////////////////
+///////////////////////////////////////////////////
+/* Copyright date */
+
 copyrightDate.textContent = new Date().getFullYear();
+
+///////////////////////////////////////////////////
+
+/* Tab component integration */
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  /* Guard clause */
+  if (!clicked) return;
+
+  /* Remove active classes */
+  tabs.forEach((tab) => tab.classList.remove('operations__tab--active'));
+
+  tabsContent.forEach((con) =>
+    con.classList.remove('operations__content--active')
+  );
+
+  /* Activate tab */
+  clicked.classList.add('operations__tab--active');
+
+  /* Activate content area */
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
